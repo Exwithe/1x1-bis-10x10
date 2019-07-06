6abaf1c2a008d7dc41db324cc27d12be231e4a9d
Exwithe <52176228+Exwithe@users.noreply.github.com>
2019-07-06 21:37:28 +0200

    Add files via upload

---
+++ Prim.java
@@ -0,0 +1,21 @@
+public class Prim {
+    public static void main(String[] args) {
+
+
+        for (int factor1 = 1;factor1 <= 10; factor1++) {
+            System.out.println("-");
+            for (int i = 0; i < 71; i++) {
+
+                System.out.print("-");
+            }
+
+            System.out.print("\n");
+
+            for (int factor2 = 1; factor2 <= 10; factor2++) {
+                System.out.print("-" + factor1 + "x" + factor2 + "=" + factor1 * factor2);
+            }
+        }
+            System.out.println();
+        }
+    }
+
